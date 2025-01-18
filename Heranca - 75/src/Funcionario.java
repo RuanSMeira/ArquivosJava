b961cfef842950ac89fdaef6664cc39b98096134
RuanSMeira <ruansilvameira@gmail.com>
2025-01-18 01:31:17 -0300

    Add files via upload

---
+++ Heranca - 75/src/Funcionario.java
@@ -0,0 +1,24 @@
+    public class Funcionario {
+        protected String nome;
+        protected String cpf;
+        protected double salario;
+        public String getNome() {
+            return nome;
+        }
+        public String getCpf() {
+            return cpf;
+        }
+        public double getSalario(){
+            return salario;
+        }
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+        public void setCpf(String cpf) {
+            this.cpf = cpf;
+        }
+        public void setSalario(double salario) {
+            this.salario = salario;
+        }
+    }
+
